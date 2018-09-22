dd9ed4792c4cbdfd1a91895a71f2aac71ae29873
Nguyen Cong Hau <nguyenconghau188@gmail.com>
2018-09-23 00:08:09 +0700

    * add basecontroller to check Login * add layout

---
+++ DOTNET/OnlineShop/OnlineShop/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = (UserLogin)Session[CommonConstant.USER_SESSION];
+            if (session == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(
+                    new { controller = "Login", action = "Login", Area = "Admin"}));
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
